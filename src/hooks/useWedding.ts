de32542b4a2604261eb4d042245ee4ae0ee40b49
heewonhong <hhw981208@naver.com>
2025-04-16 15:52:07 +0900

    [Fix] 재수정

--- src/hooks/useWedding.ts
+++ src/hooks/useWedding.ts
@@ -5,18 +5,11 @@ import { useQuery } from 'react-query'
 function useWedding() {
   const { data, isLoading, error } = useQuery<Wedding>(
     ['wedding'],
-    () =>
-      getWedding().then((res) => {
-        if (res.ok === false) {
-          throw new Error('청첩장 정보를 불러오지 못했습니다.')
-        }
-        return res.json()
-      }),
+    getWedding,
     {
       suspense: true,
     },
   )
-
   return { wedding: data, isLoading, error }
 }
 
